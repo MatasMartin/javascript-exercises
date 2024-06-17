fb78e171aa7576efb4c2a866ca4ab1eed1adc8f6
MatasMartin <mmatas.martinavicius@gmail.com>
2024-06-18 02:26:07 +0300

    Finish 3 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let temp = "";
+    for(let i = 0; i < num; i++){
+        temp = temp + string;
+    }
+    if(num < 0) {
+        return("ERROR")
+    }else {
+        return(temp)
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let letter = "a";
+    let lengthOf = 0;
+    let answer = "";
+    lengthOf = text.length;
 
+    for(let i = 0; i < lengthOf; i++){
+        letter = text.charAt((lengthOf-1)-i)
+        answer = answer + letter;
+    }
+
+    return(answer);
 };
 
 // Do not edit below this line
